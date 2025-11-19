2d2038aec593529b3869723f881b0dc6ef3fe2f5
sbraunstein42 <sbraunstein@neit.edu>
2025-11-19 11:15:18 -0500

    Almost there!

--- code.js
+++ code.js
@@ -1,24 +1,21 @@
+import { Game } from "./states/game.js";
+import { GameOver } from "./states/gameOver.js";
+import { Title } from "./states/title.js";
 import { Toolbox } from "./toolbox.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-// let myFavoriteLetters = ["a", "b", "c"];
-console.log(toolbox.getRandomColor());
+//make some states to go to.
+let game = new Game();
+let gameOver = new GameOver();
+let title = new Title();
 
-pencil.fillStyle = toolbox.getRandomColor();
-pencil.fillRect(50, 50, 100, 100);
+let state = title;
 
-// let counts = [0,0,0];
-// for(let i = 0; i < 100; i++) {
-// //     let randomIndex = getRandomIndex(myFavoriteLetters);
-// //     counts[randomIndex] += 1;
+function gameLoop() {
+    state.update();
+}
 
-//     console.log(toolbox.getRandomItem(myFavoriteLetters));
-// }
-
-
-// let shuffled = toolbox.shuffleArray(myFavoriteLetters);
-// console.log(shuffled);
-
+setInterval(gameLoop, 1000 / 60);
